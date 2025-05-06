131dbfa74a832c1fc211e58a675e7a0cd2d981ad
dntlr2000 <dntlr2000@naver.com>
2025-05-06 23:13:09 +0900

    수정
    이동 관련 문제 수정
    1인칭 캐릭터와 3인칭 캐릭터 위치 어긋남 수정
    그 외 잡다한거 다듬기

--- Assets/MYJ/PlayerState/PlayerMoveState.cs
+++ Assets/MYJ/PlayerState/PlayerMoveState.cs
@@ -22,16 +22,20 @@ public class PlayerMoveState : PlayerState
         HandleMovement();
     }
 
-    protected virtual void HandleMovement()
+    protected override void HandleMovement() //virtual로 되어 있길래 override로 수정
     {
         Vector3 move = moveDirection * player.moveSpeed;
         player.rb.linearVelocity = new Vector3(move.x, player.rb.linearVelocity.y, move.z);
 
+        /*
+         //캐릭터가 바라보는 방향을 조절하는 스크립트. 이걸 빼고나면 PlayerState의 HandleMovement()와는 차이점이 없음. 
+        // 이후에도 다른 스크립트가 추가되지 않을 예정이면 삭제하는 것이 좋아보임
         if(moveDirection != Vector3.zero)
         {
             Quaternion lookRotation = Quaternion.LookRotation(moveDirection);
             player.transform.rotation = Quaternion.Slerp(player.transform.rotation, lookRotation, 10f * Time.deltaTime);
         }
+        */
     }
 
     public override void Exit()
